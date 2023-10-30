8cd6ae3ee822343ddde42ad6339dd9f752187d7b
kamijoucen <kamijoucen@foxmail.com>
2023-10-31 00:09:00 +0800

    Initial commit

---
+++ src/main/java/com/kamijoucen/batchtask/Main.java
@@ -0,0 +1,8 @@
+package com.kamijoucen.batchtask;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("");
+    }
+}
---
+++ src/test/java/com/kamijoucen/batchtask/test/BaseTest.java
@@ -0,0 +1,12 @@
+package com.kamijoucen.batchtask.test;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+}
